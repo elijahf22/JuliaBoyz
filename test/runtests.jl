6d79e2f865284b19bcde6a191efeda12a08c4988
elijahf22 <elijahf@mit.edu>
2022-11-22 23:18:20 -0500

    Added timing data, fixed city_short

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,20 @@ using Accessors
 
 @testset "JuliaBoyz.jl" begin
     city_long = HashCode2014.read_city()
-    city_short = HashCode2014.read_city()
-    @set city_short.total_duration = 18000
+    city_short = @set city_long.total_duration = 18000
     
+    long_start = time_ns()
     solution1 = Solution(JuliaBoyz.generate_greedy_random_solution(city_long))
     upper_long = JuliaBoyz.generate_upper_bound(city_long)
+    long_end = time_ns()
+    long_time = (long_end - long_start)/10^9
+
+    short_start = time_ns()
     solution2 = Solution(JuliaBoyz.generate_greedy_random_solution(city_short))
     upper_short = JuliaBoyz.generate_upper_bound(city_short)
-    
+    short_end = time_ns()
+    short_time = (short_end - short_start) / 10^9
+
     @test HashCode2014.is_feasible(solution1, city_long)
     @test HashCode2014.is_feasible(solution2, city_short)
     @test upper_long > 0
@@ -25,9 +31,15 @@ using Accessors
     println(HashCode2014.total_distance(solution1, city_long))
     print("54000s upper bound: ")
     println(upper_long)
+    print("54000s time: ")
+    println(long_time)
+    println()
     print("18000s distance: ")
     println(HashCode2014.total_distance(solution2, city_short))
     print("18000s upper bound: ")
     println(upper_short)
+    print("18000s time: ")
+    println(short_time)
+    println()
     
 end
