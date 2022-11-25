1ba66e7b2223df254f610bb73322a96b3eca89b5
elijahf22 <elijahf@mit.edu>
2022-11-24 21:22:36 -0500

    Added testing playground

---
+++ test/playground.jl
@@ -0,0 +1,22 @@
+using JuliaBoyz
+using Test
+using Aqua
+using HashCode2014
+using Accessors
+
+
+println(city.starting_junction)
+generate_greedy_random_solution()
+println(generate_greedy_random_solution())
+
+println(city.total_duration)
+before = time_ns()
+solu = Solution(generate_greedy_random_solution())
+after = time_ns()
+print("Computation time: ")
+println((after-before)*(10^(-9)))
+println(HashCode2014.total_distance(solu, city))
+println(HashCode2014.is_feasible(solu, city; verbose=true))
+HashCode2014.write_solution(solu, "sol.txt")
+HashCode2014.plot_streets(city, solu; path="testsol.html")
+println(generate_upper_bound())
