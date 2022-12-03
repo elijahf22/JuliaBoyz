c76715c720ceb97cb483560217c7205b5c533acd
raulzinho15 <43864846+raulzinho15@users.noreply.github.com>
2022-12-03 15:46:11 -0500

    Added custom structs to JuliaBoys.jl

--- src/JuliaBoyz.jl
+++ src/JuliaBoyz.jl
@@ -8,4 +8,26 @@ include("graph_representation.jl")
 include("greedy_dfs.jl")
 include("upper_bound.jl")
 
+struct JBStreet
+    endpointA::Int64
+    endpointB::Int64
+    bidirectional::Bool
+    duration::Int64
+    distance::Int64
+end
+
+struct JBCity
+    total_duration::Int64
+    nb_cars::Int64
+    starting_junction::Int64
+    num_junctions::Int64
+    streets::Vector{JBStreet}
+end
+
+struct JBSolution
+    city::JBCity
+    itineraries::Vector{Vector{Int64}}
+    visited_streets::Set{JBStreet}
+end
+
 end
