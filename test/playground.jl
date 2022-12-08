9b4ae8ebdcb21df8fdd991fe10f4569fa4f25855
elijahf22 <elijahf@mit.edu>
2022-12-08 17:01:40 -0500

    Added playground.jl to gitignore

--- test/playground.jl
+++ test/playground.jl
@@ -1,23 +1,6 @@
-using JuliaBoyz
-using Test
-using Aqua
-using HashCode2014
-using Accessors
-using DataStructures
+using Main.JuliaBoyz
 
+problem = JuliaBoyz.read_city("test_city_long.txt")
+s1 = JuliaBoyz.greedy_dfs_solution(problem)
+JuliaBoyz.write_solution(s1, "test_solution1.txt")
 
-println(city.starting_junction)
-generate_greedy_random_solution()
-println(generate_greedy_random_solution())
-
-println(city.total_duration)
-before = time_ns()
-solu = Solution(generate_greedy_random_solution())
-after = time_ns()
-print("Computation time: ")
-println((after-before)*(10^(-9)))
-println(HashCode2014.total_distance(solu, city))
-println(HashCode2014.is_feasible(solu, city; verbose=true))
-HashCode2014.write_solution(solu, "sol.txt")
-HashCode2014.plot_streets(city, solu; path="testsol.html")
-println(generate_upper_bound())
